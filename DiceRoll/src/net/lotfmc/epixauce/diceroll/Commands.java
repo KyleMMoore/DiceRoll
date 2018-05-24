8e22c68a52a8561a79650855c8ac47e48b7bbbea
Kyle Moore <30905934+KyleMMoore@users.noreply.github.com>
2018-05-23 22:29:27 -0500

    Simplified Decision Structures
    Replaced if/else chain with a switch statement. Way easier to look at.

--- DiceRoll/src/net/lotfmc/epixauce/diceroll/Commands.java
+++ DiceRoll/src/net/lotfmc/epixauce/diceroll/Commands.java
@@ -1,6 +1,5 @@
 package net.lotfmc.epixauce.diceroll;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,114 +13,166 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Commands implements CommandExecutor{
+public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String s, String[] sRay) {
-		if(sender instanceof Player){
+		if (sender instanceof Player) {
+
 			Player player = (Player) sender;
+			String name = player.getDisplayName();
+
+			// Random obj
 			Random dice = new Random();
-			int rolled; //resultant of dice roll
-			int userDice = 0; //dice user chooses
-			List<Integer>rolls = new ArrayList<Integer>(); //stores multiple dice rolls
-			
-			if(sRay.length==0){
-				player.sendMessage(ChatColor.RED+"Try /roll <diceType>"); //when user fails to pick a dice
+			// resultant of dice roll
+			int rolled;
+			// dice user chooses
+			int userDice = 0;
+			// stores multiple dice rolls
+			List<Integer> rolls = new ArrayList<Integer>();
+			switch (sRay.length) {
+			// when user fails to pick a dice
+			case 0:
+				player.sendMessage(ChatColor.RED + "Try /roll <diceType>");
 				return true;
-			}else if(sRay.length ==1){ //if command has at least one additional argument
-			//probably could be done better, this try/catch validates Integer.parseInt()	
-			try{
-				if(Integer.parseInt(sRay[0]) <= 100 && Integer.parseInt(sRay[0]) >= 2){ //dice must be between 2 and 100
-					userDice = Integer.parseInt(sRay[0]); //sets the dice
-					rolled = dice.nextInt(userDice)+1; //"rolls" the dice
-				}else{
-					player.sendMessage(ChatColor.RED+"That dice has too many/too few sides, "+player.getDisplayName() + "!"); //returned if dice is outside range
+			// when user only enters one dice
+			case 1:
+				try {
+					// dice must be between 2 and 100
+					if (Integer.parseInt(sRay[0]) <= 100 && Integer.parseInt(sRay[0]) >= 2) {
+
+						userDice = Integer.parseInt(sRay[0]); // sets the dice
+						rolled = dice.nextInt(userDice) + 1; // "rolls" the dice
+
+					} else {
+
+						// returned if dice is outside range
+						player.sendMessage(ChatColor.RED + "That dice has too many/too few sides, " + name + "!");
+						return true;
+
+					}
+					// DISPLAY RESULTS
+					displayResults(player, rolled, userDice);
+
+				} catch (NumberFormatException e) {
+					// returned if dice is outside range
+					player.sendMessage(ChatColor.RED + "That's no dice, " + name);
 					return true;
 				}
-				displayResults(player,rolled,userDice); //DISPLAY RESULTS
-				
-			} catch(NumberFormatException e){
-				player.sendMessage(ChatColor.RED+"That's no dice, "+player.getDisplayName()); //returned if dice is outside range
 				return true;
-			}
-
-			
-			
-			//A-ok!
-			return true;			
-			}else if(sRay.length ==2) {
+			// when user throws multiple dice
+			case 2:
 				try {
-					if(Integer.parseInt(sRay[0]) <= 100 && Integer.parseInt(sRay[0]) >= 2){ //dice must be between 2 and 100
-						userDice = Integer.parseInt(sRay[0]); //sets the dice
-						rolled = dice.nextInt(userDice)+1; //"rolls" the dice
-					}else {
-						player.sendMessage(ChatColor.RED+"That dice has too many/too few sides, "+player.getDisplayName() + "!"); //returned if dice is outside range
+					// dice must be between 2 and 100
+					if (Integer.parseInt(sRay[0]) <= 100 && Integer.parseInt(sRay[0]) >= 2) {
+
+						// sets the dice
+						userDice = Integer.parseInt(sRay[0]);
+						// "rolls" the dice
+						rolled = dice.nextInt(userDice) + 1;
+
+					} else {
+
+						// returned if dice is outside range
+						player.sendMessage(ChatColor.RED + "That dice has too many/too few sides, " + name + "!");
 						return false;
+
 					}
-					if(Integer.parseInt(sRay[1]) <= 12 && Integer.parseInt(sRay[1]) >= 2) { //dice amount must be be from 1-12
-						for(int a = 0; a<Integer.parseInt(sRay[1]); a++) {
+					// dice amount must be be from 1-12
+					if (Integer.parseInt(sRay[1]) <= 12 && Integer.parseInt(sRay[1]) >= 2) {
+
+						for (int a = 0; a < Integer.parseInt(sRay[1]); a++) {
 							userDice = Integer.parseInt(sRay[0]);
-							rolls.add(dice.nextInt(userDice)+1);
+							rolls.add(dice.nextInt(userDice) + 1);
 						}
 						displayResults(player, rolls, userDice);
 						return true;
-					}else if(Integer.parseInt(sRay[1]) == 1){
-						player.sendMessage(ChatColor.RED+"This command is for multiple dice! Try /roll <diceType>");
+
+					} else if (Integer.parseInt(sRay[1]) == 1) {
+
+						player.sendMessage(ChatColor.RED + "This command is for multiple dice! Try /roll <diceType>");
 						return true;
-					}else {
-						player.sendMessage(ChatColor.RED+"Invalid dice amount, try again "+player.getDisplayName()); //returned if too many/ too few dice are rolled
+
+					} else {
+
+						// returned if too many/ too few dice are rolled
+						player.sendMessage(ChatColor.RED + "Invalid dice amount, try again " + name);
 						return false;
+
 					}
-				}catch(NumberFormatException e) {
-					player.sendMessage(ChatColor.RED+"That's no dice, "+player.getDisplayName()); //returned if dice is outside range
-					return true;					
+				} catch (NumberFormatException e) {
+					// returned if dice is outside range
+					player.sendMessage(ChatColor.RED + "That's no dice, " + name);
+					return true;
 				}
-			}else{
-				player.sendMessage(ChatColor.RED+"Try /roll <diceType>"); //when user sends invalid command
+			// when user tries /roll without a dice type
+			default:
+				// when user sends invalid command
+				player.sendMessage(ChatColor.RED + "Try /roll <diceType>");
 				return true;
 			}
-			
-			}else{ //else for player check
-		Bukkit.getConsoleSender().sendMessage("Command must be sent from player!");
-			return false;				
-			}
-	  }
-	
+		} else {
+
+			Bukkit.getConsoleSender().sendMessage("Command must be sent from player!");
+			return false;
+
+		}
+	}
+
 	private void displayResults(Player player, int rolled, int userDice) {
-		Location loc = player.getLocation(); //player who rolls dice's location
-		Location target; //potential location of players nearby
-		
-		for(Player p: player.getWorld().getPlayers()){ //runs through all players online to check their relation to sender
-			target = p.getLocation(); //grabs location of player in list
-			if(loc.distance(target)<=20){ //20 is the max distance between sender and recipients
-				
-				if(p.equals(player)){//if player is you
-					//final product "You rolled a 4 on a D6"
-					p.sendMessage(ChatColor.GOLD+"You rolled a "+rolled+ " on a D"+userDice);
-				}else{
-					//final product "Epixauce rolled a 4 on a D6"
-					p.sendMessage(ChatColor.GOLD+player.getDisplayName()+" rolled a "+rolled+" on a D"+userDice);						
+		// player who rolls dice's location
+		Location loc = player.getLocation();
+		// potential location of players nearby
+		Location target;
+		// runs through all players online to check their relation to sender
+		for (Player p : player.getWorld().getPlayers()) {
+			// grabs location of player in list
+			target = p.getLocation();
+			// 20 is the max distance between sender and recipients
+			if (loc.distance(target) <= 20) {
+
+				// if player is you
+				if (p.equals(player)) {
+
+					// final product "You rolled a 4 on a D6"
+					p.sendMessage(ChatColor.GOLD + "You rolled a " + rolled + " on a D" + userDice);
+				} else {
+
+					// final product "Epixauce rolled a 4 on a D6"
+					p.sendMessage(
+							ChatColor.GOLD + player.getDisplayName() + " rolled a " + rolled + " on a D" + userDice);
 				}
 			}
-		}	
+		}
 	}
-	private void displayResults(Player player, List<Integer>rolls, int userDice) {
-		Location loc = player.getLocation(); //player who rolls dice's location
-		Location target; //potential location of players nearby
-		String message = " rolled ["; //compiled output of all rolls
-		for(Integer i: rolls) {
+
+	private void displayResults(Player player, List<Integer> rolls, int userDice) {
+		// player who rolls dice's location
+		Location loc = player.getLocation();
+		// potential location of players nearby
+		Location target;
+		// compiled output of all rolls
+		String message = " rolled [";
+		for (Integer i : rolls) {
 			message += " " + i;
 		}
 		message += " ]";
-		
-		for(Player p: player.getWorld().getPlayers()){ //runs through all players online to check their relation to sender
-			target = p.getLocation(); //grabs location of player in list
-			if(loc.distance(target)<=20){ //10 is the max distance between sender and recipients
-				
-				if(p.equals(player)){//if player is you
-					//final product "You rolled [ n n n n n n ] on 6 D6"
-					p.sendMessage(ChatColor.GOLD+"You"+message + " on " +rolls.size() + " D"+userDice);
-				}else{
-					//final product "Epixauce rolled [ n n n n n n ] on 6 D6"
-					p.sendMessage(ChatColor.GOLD+player.getDisplayName() + message + " on " +rolls.size() + " D"+userDice);				
+		// runs through all players online to check their relation to sender
+		for (Player p : player.getWorld().getPlayers()) {
+			// grabs location of player in list
+			target = p.getLocation();
+			// 10 is the max distance between sender and recipients
+			if (loc.distance(target) <= 20) {
+
+				// if player is you
+				if (p.equals(player)) {
+
+					// final product "You rolled [ n n n n n n ] on 6 D6"
+					p.sendMessage(ChatColor.GOLD + "You" + message + " on " + rolls.size() + " D" + userDice);
+				} else {
+
+					// final product "Epixauce rolled [ n n n n n n ] on 6 D6"
+					p.sendMessage(ChatColor.GOLD + player.getDisplayName() + message + " on " + rolls.size() + " D"
+							+ userDice);
+
 				}
 			}
 		}
